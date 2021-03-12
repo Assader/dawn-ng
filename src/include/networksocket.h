7146ffae57eb9d2b82f9c362d161f3c56ec4e0ac
Markov Mikhail <markov.mikhail@itmh.ru>
2021-03-13 01:58:19 +0500

    networksocket: do basic clean up
    Signed-off-by: Markov Mikhail <markov.mikhail@itmh.ru>

--- src/include/networksocket.h
+++ src/include/networksocket.h
@@ -1,16 +1,16 @@
-#ifndef __DAWN_NETWORKSOCKET_H
-#define __DAWN_NETWORKSOCKET_H
+#ifndef DAWN_NETWORKSOCKET_H
+#define DAWN_NETWORKSOCKET_H
 
 #include <pthread.h>
 
 /**
  * Init a socket using the runopts.
- * @param _ip - ip to use.
- * @param _port - port to use.
- * @param _multicast_socket - if socket should be multicast or broadcast.
+ * @param ip - ip to use.
+ * @param port - port to use.
+ * @param multicast_socket - if socket should be multicast or broadcast.
  * @return the socket.
  */
-int init_socket_runopts(const char *_ip, int _port, int _multicast_socket);
+int init_socket_runopts(const char *ip, int port, int multicast_socket);
 
 /**
  * Send message via network.
@@ -29,9 +29,6 @@ int send_string_enc(char *msg);
 /**
  * Close socket.
  */
-void close_socket();
+void close_socket(void);
 
-// save connections
-// struct sockaddr_in addr[100];
-
-#endif
+#endif /* DAWN_NETWORKSOCKET_H */
