5f9daa4cb73ce4930ec85fe073efd0352fd60aa3
Markov Mikhail <markov.mikhail@itmh.ru>
2021-03-14 04:10:11 +0500

    multicastsocket: polish code a lil
    Signed-off-by: Markov Mikhail <markov.mikhail@itmh.ru>

--- src/network/multicastsocket.c
+++ src/network/multicastsocket.c
@@ -20,7 +20,7 @@ int setup_multicast_socket(const char *multicast_ip, unsigned short multicast_po
     /* Allow multiple processes to use the same port */
     int reuse_addr = 1;
     if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR,
-                   &reuse_addr, sizeof (reuse_addr)) < 0) {
+                   &reuse_addr, sizeof (reuse_addr)) != 0) {
         perror("Failed to set SO_REUSEADDR option to socket");
         return -1;
     }
@@ -28,7 +28,7 @@ int setup_multicast_socket(const char *multicast_ip, unsigned short multicast_po
     /* When using this option, multicast will be looped back to out host */
     int multicast_loop = 1;
     if (setsockopt(sock, IPPROTO_IP, IP_MULTICAST_LOOP,
-                   &multicast_loop, sizeof (multicast_loop)) < 0) {
+                   &multicast_loop, sizeof (multicast_loop)) != 0) {
         perror("Failed to set IP_MULTICAST_LOOP option to socket");
         return -1;
     }
@@ -38,7 +38,7 @@ int setup_multicast_socket(const char *multicast_ip, unsigned short multicast_po
     addr->sin_addr.s_addr = inet_addr(multicast_ip);
     addr->sin_port = htons(multicast_port);
 
-    if (bind(sock, (struct sockaddr *) addr, sizeof (*addr)) < 0) {
+    if (bind(sock, (struct sockaddr *) addr, sizeof (*addr)) != 0) {
         perror("Socket binding failed");
         return -1;
     }
@@ -47,7 +47,7 @@ int setup_multicast_socket(const char *multicast_ip, unsigned short multicast_po
     command.imr_multiaddr.s_addr = inet_addr(multicast_ip);
     command.imr_interface.s_addr = htonl(INADDR_ANY);
     if (setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP,
-                   &command, sizeof(command)) < 0) {
+                   &command, sizeof(command)) != 0) {
         perror("Failed to join multicast group");
         return -1;
     }
@@ -60,7 +60,7 @@ int drop_multicast_group_membership(int sock)
     int err;
 
     err = setsockopt(sock, IPPROTO_IP, IP_DROP_MEMBERSHIP, &command, sizeof (command));
-    if (err < 0) {
+    if (err != 0) {
         perror("Failed to drop multicast group membership");
     }
 
