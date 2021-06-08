bd56c3a13d687a1d60a7e68f203ab8715ea40084
Markov Mikhail <markov.mikhail@itmh.ru>
2021-06-08 16:51:56 +0500

    dawn_log: extend debug logs with additional information
    Signed-off-by: Markov Mikhail <markov.mikhail@itmh.ru>

--- src/include/dawn_log.h
+++ src/include/dawn_log.h
@@ -9,12 +9,22 @@ enum {
     DAWN_LOG_LEVEL_DEBUG   = 4,
 };
 
+#ifdef DEBUG
+#define DAWN_LOG_ERROR(...)     dawn_log(DAWN_LOG_LEVEL_ERROR, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#define DAWN_LOG_WARNING(...)   dawn_log(DAWN_LOG_LEVEL_WARNING, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#define DAWN_LOG_INFO(...)      dawn_log(DAWN_LOG_LEVEL_INFO, __FILE__, __func__, __LINE__, __VA_ARGS__)
+#define DAWN_LOG_DEBUG(...)     dawn_log(DAWN_LOG_LEVEL_DEBUG, __FILE__, __func__, __LINE__, __VA_ARGS__)
+
+void dawn_log(int log_level, const char *file, const char *function, int line, const char *format, ...);
+#else
 #define DAWN_LOG_ERROR(...)     dawn_log(DAWN_LOG_LEVEL_ERROR, __VA_ARGS__)
 #define DAWN_LOG_WARNING(...)   dawn_log(DAWN_LOG_LEVEL_WARNING, __VA_ARGS__)
 #define DAWN_LOG_INFO(...)      dawn_log(DAWN_LOG_LEVEL_INFO, __VA_ARGS__)
 #define DAWN_LOG_DEBUG(...)     dawn_log(DAWN_LOG_LEVEL_DEBUG, __VA_ARGS__)
 
-void dawn_set_log_level(int log_level);
 void dawn_log(int log_level, const char *format, ...);
+#endif
+void dawn_set_log_level(int log_level);
+
 
 #endif /* DAWN_LOG_H */
--- src/utils/dawn_log.c
+++ src/utils/dawn_log.c
@@ -8,20 +8,43 @@
 
 static int dawn_log_level = DAWN_LOG_LEVEL_WARNING;
 
-void dawn_set_log_level(int log_level)
+#ifdef DEBUG
+void dawn_log(int log_level, const char *file, const char *function, int line, const char *format, ...)
 {
-    dawn_log_level = log_level;
-}
+    char *message;
+    va_list arg_list;
 
+    if (log_level <= dawn_log_level) {
+        va_start(arg_list, format);
+        vasprintf(&message, format, arg_list);
+        va_end(arg_list);
+
+#ifdef DAWN_LOG_TO_SYSLOG
+        int log_level_map[] = {
+            [DAWN_LOG_LEVEL_DEBUG]   = LOG_DEBUG,
+            [DAWN_LOG_LEVEL_INFO]    = LOG_INFO,
+            [DAWN_LOG_LEVEL_WARNING] = LOG_WARNING,
+            [DAWN_LOG_LEVEL_ERROR]   = LOG_ERR,
+        };
+
+        syslog(log_level_map[log_level], "%s:%s:%d: %s", file, function, line, message);
+#else
+        fprintf(stderr, "%s:%s:%d: %s\n", file, function, line, message);
+#endif
+
+        free(message);
+    }
+}
+#else
 void dawn_log(int log_level, const char *format, ...)
 {
     char *message;
-    va_list ptr;
+    va_list arg_list;
 
     if (log_level <= dawn_log_level) {
-        va_start(ptr, format);
-        vasprintf(&message, format, ptr);
-        va_end(ptr);
+        va_start(arg_list, format);
+        vasprintf(&message, format, arg_list);
+        va_end(arg_list);
 
 #ifdef DAWN_LOG_TO_SYSLOG
         int log_level_map[] = {
@@ -38,4 +61,10 @@ void dawn_log(int log_level, const char *format, ...)
 
         free(message);
     }
+}
+#endif
+
+void dawn_set_log_level(int log_level)
+{
+    dawn_log_level = log_level;
 }
